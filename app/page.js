abebdf4ac947a34f088870f772ea8fab5b5f2b00
Dan Dyar <dan@parameterz.com>
2024-11-11 10:37:31 -0800

    Update page.js
    this works, but the styling is minimal

--- app/page.js
+++ app/page.js
@@ -2,12 +2,5 @@
 import ValveReference from '@/components/ValveReference'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-8">
-      <h1 className="text-3xl font-bold mb-8">
-        Prosthetic Valve Reference Tool
-      </h1>
-      <ValveReference />
-    </main>
-  )
+  return <ValveReference />
 }
